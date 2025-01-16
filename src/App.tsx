1dbb6bc883053a26702eb32c5fdde273394feb9a
Jaque <jaquelinecosta-rj@hotmail.com>
2025-01-16 20:28:39 -0300

    pagina de cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
 import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
-import Home from './pages/home/Home'
-import './App.css'
+import Cadastro from './pages/cadastro/Cadastro'
 
 function App() {
   return (
@@ -11,8 +11,8 @@ function App() {
           <Navbar />
           <div className="min-h-[80vh]">
             <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/home" element={<Home />} />
+            {/* <Route path="/" elemen{<Home />} /><Route path="/home" element={<Home />} /> */}
+             <Route path="/" element={<Cadastro />} />
             </Routes>
           </div>
           <Footer />
