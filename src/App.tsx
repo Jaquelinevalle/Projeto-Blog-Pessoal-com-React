4ced0b96e2b82f4b59cb9489a0d9b87b813160f0
Jaque <jaquelinecosta-rj@hotmail.com>
2025-01-16 20:37:57 -0300

    pagina de login

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
+import Home from './pages/home/Home'
+import Login from './pages/login/Login'
 import Cadastro from './pages/cadastro/Cadastro'
 
 function App() {
@@ -11,8 +13,10 @@ function App() {
           <Navbar />
           <div className="min-h-[80vh]">
             <Routes>
-            {/* <Route path="/" elemen{<Home />} /><Route path="/home" element={<Home />} /> */}
-             <Route path="/" element={<Cadastro />} />
+              <Route path="/" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/login" element={<Login />} />
             </Routes>
           </div>
           <Footer />
